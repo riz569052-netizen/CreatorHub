d1d3cdbd08c74962d0a987b0718cd2542f874511
riz569052-netizen <riz569052@gmail.com>
2026-07-23 12:28:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,50 @@
+// Elements
 
+const authContainer = document.querySelector(".auth-container");
+const dashboard = document.querySelector(".dashboard");
+
+const loginBtn = document.querySelector(".main-btn");
+const logoutBtn = document.querySelector(".logout");
+
+const emailInput = document.querySelector('input[type="email"]');
+const passwordInput = document.querySelector('input[type="password"]');
+
+
+// Login
+
+loginBtn.addEventListener("click", () => {
+
+    const email = emailInput.value.trim();
+    const password = passwordInput.value.trim();
+
+    if(email === "" || password === ""){
+        alert("Please enter Email and Password.");
+        return;
+    }
+
+    authContainer.style.display = "none";
+    dashboard.style.display = "block";
+
+});
+
+
+// Logout
+
+logoutBtn.addEventListener("click", () => {
+
+    dashboard.style.display = "none";
+    authContainer.style.display = "flex";
+
+    emailInput.value = "";
+    passwordInput.value = "";
+
+});
+
+
+// Google Button Demo
+
+const googleBtn = document.querySelector(".google-btn");
+
+googleBtn.addEventListener("click", () => {
+    alert("Google Login will be connected with Firebase Authentication.");
+});
