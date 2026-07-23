471201dcd1a22a5a6cc1fcdfde6b342abff07ee6
riz569052-netizen <riz569052@gmail.com>
2026-07-23 08:23:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,88 @@
+*{
+    margin:0;
+    padding:0;
+    box-sizing:border-box;
+    font-family:Arial,sans-serif;
+}
 
+body{
+    background:#0f172a;
+    color:#fff;
+}
+
+nav{
+    background:#111827;
+    padding:18px;
+    text-align:center;
+    font-size:28px;
+    font-weight:bold;
+    color:#38bdf8;
+    box-shadow:0 3px 10px rgba(0,0,0,.3);
+}
+
+.hero{
+    text-align:center;
+    padding:50px 20px;
+}
+
+.hero h1{
+    font-size:38px;
+    margin-bottom:15px;
+}
+
+.hero p{
+    color:#cbd5e1;
+    font-size:18px;
+}
+
+.tools{
+    width:90%;
+    max-width:700px;
+    margin:30px auto;
+}
+
+.card{
+    background:#1e293b;
+    padding:25px;
+    border-radius:18px;
+    margin-bottom:25px;
+    box-shadow:0 8px 25px rgba(0,0,0,.35);
+}
+
+.card h2{
+    margin-bottom:15px;
+    color:#38bdf8;
+}
+
+input{
+    width:100%;
+    padding:14px;
+    border:none;
+    border-radius:10px;
+    margin-top:10px;
+    margin-bottom:15px;
+    font-size:16px;
+}
+
+button{
+    width:100%;
+    padding:14px;
+    border:none;
+    border-radius:10px;
+    background:#0ea5e9;
+    color:#fff;
+    font-size:17px;
+    font-weight:bold;
+    cursor:pointer;
+    transition:.3s;
+}
+
+button:hover{
+    background:#0284c7;
+}
+
+footer{
+    text-align:center;
+    padding:25px;
+    color:#94a3b8;
+      }
